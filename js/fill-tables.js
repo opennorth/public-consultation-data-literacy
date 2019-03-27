59a4423961c5bbe4fa8c423f9306fa667b48dc31
hannahker <hannah.ker96@gmail.com>
2019-03-27 17:01:06 -0400

    undate dataset layout

--- js/fill-tables.js
+++ js/fill-tables.js
@@ -21,16 +21,19 @@ function getData(source){
 					//create Tabulator on DOM element with id "example-table"
 					var table = new Tabulator("#example-table", {
 					 	height:500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+					 	layout: 'fitColumns',
+					 	tooltipsHeader: true,
+					 	tooltips: true,
 					 	data:data, //assign data to table
 					 	columns:[ //Define Table Columns
-					 		{title:colHeaders[0], field: colHeaders[0], width: 40, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[1], field:colHeaders[1], width: 80, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[2], field:colHeaders[2], width: 80, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[3], field:colHeaders[3], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[4], field:colHeaders[4], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[5], field:colHeaders[5], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[6], field:colHeaders[6], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[7], field:colHeaders[7], width: 120, headerSort:false, formatter: "textarea"},
+					 		{title:colHeaders[0], field: colHeaders[0], headerSort:false},
+						 	{title:colHeaders[1], field:colHeaders[1], headerSort:false},
+						 	{title:colHeaders[2], field:colHeaders[2], headerSort:false},
+						 	{title:colHeaders[3], field:colHeaders[3], headerSort:false},
+						 	{title:colHeaders[4], field:colHeaders[4],  headerSort:false},
+						 	{title:colHeaders[5], field:colHeaders[5],  headerSort:false},
+						 	{title:colHeaders[6], field:colHeaders[6], headerSort:false},
+						 	{title:colHeaders[7], field:colHeaders[7],  headerSort:false},
 					 	],
 
 					});
@@ -95,24 +98,26 @@ function getData(source){
 					//create Tabulator on DOM element with id "example-table"
 					var table = new Tabulator("#quiz1-table", {
 					 	height:500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+					 	tooltipsHeader: true,
+					 	tooltips: true,
 					 	data:data, //assign data to table
 					 	columns:[ //Define Table Columns
-					 		{title:colHeaders[0], field: colHeaders[0], width: 80, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[1], field:colHeaders[1], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[2], field:colHeaders[2], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[3], field:colHeaders[3], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[4], field:colHeaders[4], width: 80, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[5], field:colHeaders[5], width: 200, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[6], field:colHeaders[6], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[7], field:colHeaders[7], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[8], field:colHeaders[8], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[9], field:colHeaders[9], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[10], field:colHeaders[10], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[11], field:colHeaders[11], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[12], field:colHeaders[12], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[13], field:colHeaders[13], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[14], field:colHeaders[14], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[15], field:colHeaders[15], width: 120, headerSort:false, formatter: "textarea"}
+					 		{title:colHeaders[0], field: colHeaders[0], width: 80, headerSort:false},
+						 	{title:colHeaders[1], field:colHeaders[1], width: 120, headerSort:false},
+						 	{title:colHeaders[2], field:colHeaders[2], width: 120, headerSort:false},
+						 	{title:colHeaders[3], field:colHeaders[3], width: 120, headerSort:false},
+						 	{title:colHeaders[4], field:colHeaders[4], width: 80, headerSort:false},
+						 	{title:colHeaders[5], field:colHeaders[5], width: 200, headerSort:false},
+						 	{title:colHeaders[6], field:colHeaders[6], width: 120, headerSort:false},
+						 	{title:colHeaders[7], field:colHeaders[7], width: 120, headerSort:false},
+						 	{title:colHeaders[8], field:colHeaders[8], width: 120, headerSort:false},
+						 	{title:colHeaders[9], field:colHeaders[9], width: 120, headerSort:false},
+						 	{title:colHeaders[10], field:colHeaders[10], width: 120, headerSort:false},
+						 	{title:colHeaders[11], field:colHeaders[11], width: 120, headerSort:false},
+						 	{title:colHeaders[12], field:colHeaders[12], width: 120, headerSort:false},
+						 	{title:colHeaders[13], field:colHeaders[13], width: 120, headerSort:false},
+						 	{title:colHeaders[14], field:colHeaders[14], width: 120, headerSort:false},
+						 	{title:colHeaders[15], field:colHeaders[15], width: 120, headerSort:false}
 					 	],
 
 					});	
@@ -123,28 +128,30 @@ function getData(source){
 					//create Tabulator on DOM element with id "example-table"
 					var table = new Tabulator("#quiz2-table", {
 					 	height:500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+					 	tooltipsHeader: true,
+					 	tooltips: true,
 					 	data:data, //assign data to table
 					 	columns:[ //Define Table Columns
-					 		{title:colHeaders[0], field: colHeaders[0], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[1], field:colHeaders[1], width: 300, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[2], field:colHeaders[2], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[3], field:colHeaders[3], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[4], field:colHeaders[4], width: 80, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[5], field:colHeaders[5], width: 200, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[6], field:colHeaders[6], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[7], field:colHeaders[7], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[8], field:colHeaders[8], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[9], field:colHeaders[9], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[10], field:colHeaders[10], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[11], field:colHeaders[11], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[12], field:colHeaders[12], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[13], field:colHeaders[13], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[14], field:colHeaders[14], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[15], field:colHeaders[15], width: 120, headerSort:false, formatter: "textarea"}, 
-						 	{title:colHeaders[16], field:colHeaders[16], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[17], field:colHeaders[17], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[18], field:colHeaders[18], width: 120, headerSort:false, formatter: "textarea"},
-						 	{title:colHeaders[19], field:colHeaders[19], width: 120, headerSort:false, formatter: "textarea"}
+					 		{title:colHeaders[0], field: colHeaders[0], width: 120, headerSort:false},
+						 	{title:colHeaders[1], field:colHeaders[1], width: 300, headerSort:false},
+						 	{title:colHeaders[2], field:colHeaders[2], width: 120, headerSort:false},
+						 	{title:colHeaders[3], field:colHeaders[3], width: 120, headerSort:false},
+						 	{title:colHeaders[4], field:colHeaders[4], width: 80, headerSort:false},
+						 	{title:colHeaders[5], field:colHeaders[5], width: 200, headerSort:false},
+						 	{title:colHeaders[6], field:colHeaders[6], width: 120, headerSort:false},
+						 	{title:colHeaders[7], field:colHeaders[7], width: 120, headerSort:false},
+						 	{title:colHeaders[8], field:colHeaders[8], width: 120, headerSort:false},
+						 	{title:colHeaders[9], field:colHeaders[9], width: 120, headerSort:false},
+						 	{title:colHeaders[10], field:colHeaders[10], width: 120, headerSort:false},
+						 	{title:colHeaders[11], field:colHeaders[11], width: 120, headerSort:false},
+						 	{title:colHeaders[12], field:colHeaders[12], width: 120, headerSort:false},
+						 	{title:colHeaders[13], field:colHeaders[13], width: 120, headerSort:false},
+						 	{title:colHeaders[14], field:colHeaders[14], width: 120, headerSort:false},
+						 	{title:colHeaders[15], field:colHeaders[15], width: 120, headerSort:false}, 
+						 	{title:colHeaders[16], field:colHeaders[16], width: 120, headerSort:false},
+						 	{title:colHeaders[17], field:colHeaders[17], width: 120, headerSort:false},
+						 	{title:colHeaders[18], field:colHeaders[18], width: 120, headerSort:false},
+						 	{title:colHeaders[19], field:colHeaders[19], width: 120, headerSort:false}
 					 	],
 
 					});	
@@ -155,28 +162,30 @@ function getData(source){
 					//create Tabulator on DOM element with id "example-table"
 					var table = new Tabulator("#m2-example-table", {
 					 	height:500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+					 	tooltipsHeader: true,
+					 	tooltips: true,
 					 	data:data, //assign data to table
 					 	columns:[ //Define Table Columns
-					 		{title:colHeaders[0], field: colHeaders[0], width: 80,  formatter: "textarea"},
-						 	{title:colHeaders[1], field:colHeaders[1], width: 80, formatter: "textarea"},
-						 	{title:colHeaders[2], field:colHeaders[2], width: 80, formatter: "textarea"},
-						 	{title:colHeaders[3], field:colHeaders[3], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[4], field:colHeaders[4], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[5], field:colHeaders[5], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[6], field:colHeaders[6], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[7], field:colHeaders[7], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[8], field:colHeaders[8], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[9], field:colHeaders[9], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[10], field:colHeaders[10], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[11], field:colHeaders[11], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[12], field:colHeaders[12], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[13], field:colHeaders[13], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[14], field:colHeaders[14], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[15], field:colHeaders[15], width: 200, formatter: "textarea"}, 
-						 	{title:colHeaders[16], field:colHeaders[16], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[17], field:colHeaders[17], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[18], field:colHeaders[18], width: 200, formatter: "textarea"},
-						 	{title:colHeaders[19], field:colHeaders[19], width: 200, formatter: "textarea"}
+					 		{title:colHeaders[0], field: colHeaders[0], width: 80},
+						 	{title:colHeaders[1], field:colHeaders[1], width: 80},
+						 	{title:colHeaders[2], field:colHeaders[2], width: 80},
+						 	{title:colHeaders[3], field:colHeaders[3], width: 200},
+						 	{title:colHeaders[4], field:colHeaders[4], width: 200},
+						 	{title:colHeaders[5], field:colHeaders[5], width: 200},
+						 	{title:colHeaders[6], field:colHeaders[6], width: 200},
+						 	{title:colHeaders[7], field:colHeaders[7], width: 200},
+						 	{title:colHeaders[8], field:colHeaders[8], width: 200},
+						 	{title:colHeaders[9], field:colHeaders[9], width: 200},
+						 	{title:colHeaders[10], field:colHeaders[10], width: 200},
+						 	{title:colHeaders[11], field:colHeaders[11], width: 200},
+						 	{title:colHeaders[12], field:colHeaders[12], width: 200},
+						 	{title:colHeaders[13], field:colHeaders[13], width: 200},
+						 	{title:colHeaders[14], field:colHeaders[14], width: 200},
+						 	{title:colHeaders[15], field:colHeaders[15], width: 200}, 
+						 	{title:colHeaders[16], field:colHeaders[16], width: 200},
+						 	{title:colHeaders[17], field:colHeaders[17], width: 200},
+						 	{title:colHeaders[18], field:colHeaders[18], width: 200},
+						 	{title:colHeaders[19], field:colHeaders[19], width: 200}
 					 	],
 
 					});	
@@ -187,7 +196,7 @@ function getData(source){
 					fillTable13(); 
 				}
 				if(source == "https://hannahker.github.io/web-development/data/consultation/gen-eng-subset.json"){
-					fillTable14(); 
+					fillTable14();
 				}
 				if(source == "https://hannahker.github.io/web-development/data/consultation/ntnl-scrt-sub-email.json"){
 					fillTable24();
